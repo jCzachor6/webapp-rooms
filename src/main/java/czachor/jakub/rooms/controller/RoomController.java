9f9c365e6ce9fca90974378a93df99e3a097a03e
jCzachor6 <jakubczachor7@gmail.com>
2018-09-04 09:50:44 +0200

    Changed controller getRoomByKey to return modelandview.

--- src/main/java/czachor/jakub/rooms/controller/RoomController.java
+++ src/main/java/czachor/jakub/rooms/controller/RoomController.java
@@ -4,10 +4,10 @@ import czachor.jakub.rooms.models.dto.RoomDTO;
 import czachor.jakub.rooms.service.RoomService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller("roomController")
 @RequestMapping("/room")
@@ -20,8 +20,10 @@ public class RoomController {
     }
 
     @RequestMapping(value = "/{key}")
-    public ResponseEntity<RoomDTO> getRoomByKey(@PathVariable("key") String key){
+    public ModelAndView getRoomByKey(@PathVariable("key") String key){
         RoomDTO roomDTO = roomService.getRoomByKey(key);
-        return new ResponseEntity<>(roomDTO, HttpStatus.OK);
+        ModelAndView mav = new ModelAndView("room", HttpStatus.OK);
+        mav.addObject("room", roomDTO);
+        return mav;
     }
 }
